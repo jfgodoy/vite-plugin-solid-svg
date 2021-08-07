299ee2d6473d7f9a0c7a2a7e36aba40f5e16b704
Jorge Godoy <godoy.jf@gmail.com>
2021-08-07 16:56:08 -0400

    add default value for props param

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function compileSvg(source) {
   import { template, spread } from "solid-js/web";
 
   const _tmpl$ = template(\`${source}\`, 0);
-  export default (props) => {
+  export default (props = {}) => {
     const _el$ = _tmpl$.cloneNode(true);
     spread(_el$, props, true);
     return _el$;
